293cf1b4bcae3ccce9d7d2d5699f5086a2d0cea7
bnousilal <bnousilal@gmail.com>
2022-04-16 09:55:52 -0700

    Create arthmatic_print_style.py

---
+++ arthmatic_print_style.py
@@ -0,0 +1,13 @@
+def MathOp():
+  
+  classic_division = 3/2 ## Calculate 3/2 here
+  floor_division = 3//2 ## Calculate 3//2 here
+  modulus = 3%2 ## Calculate 3%2 here 
+  power = 3**2 ## Calculate 3**2 here
+  ## Returning the calculations for evaluation
+  return [classic_division, floor_division, modulus, power]
+
+[classic_division, floor_division, modulus, power]=MathOp()
+print(*[classic_division, floor_division, modulus, power], sep="\n")
+print(f"{classic_division}\n{floor_division}\n{modulus}\n{power}")
+print("{0}\n{1}\n{2}\n{3}".format(classic_division, floor_division, modulus, power))
